48eb479428fa64bce8f44875a2fa0ec66ce08c6d
Ashley <91754686+pettysavage@users.noreply.github.com>
2022-01-24 13:10:55 -0600

    Version 2
    These files are what works for our presentation on 1/24/22

---
+++ includes/logIn.inc.php
@@ -0,0 +1,64 @@
+<?php
+    include_once 'dbh.inc.php';
+
+    $userName = $pwd = "";
+    $userName_err ="" ;
+    $pwd_err = "";
+    $login_err = "";
+//check for empty username
+    if($_SERVER["REQUEST_METHOD"]== "POST"){
+        if(empty(trim($_POST["userName"]))){
+            $userName_err = "Please enter username.";
+        }
+    } else{
+        $userName = trim($_POST["userName"]);
+    }
+//check for empty password
+if($_SERVER["REQUEST_METHOD"]== "POST"){
+    if(empty(trim($_POST["pwd"]))){
+        $pwd_err = "Please enter password.";
+    }
+} else{
+    $pwd = trim($_POST["userName"]);
+}
+
+//validate
+if(!empty($userName_err)&& !empty($pwd_err)){
+    $sql = "SELECT userID, userName, password FROM user WHERE userName = ? ;";
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+        mysqli_stmt_bind_param($stmt, "s", $param_userName);
+
+        $param_userName = $userName;
+
+        //execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            //store result
+            mysqli_stmt_store_result($stmt);
+
+            //check if userName exists --> if yes verify pwd
+            if(mysqli_stmt_num_rows($stmt)== 1){
+                
+                //bind variables
+                mysqli_stmt_bind_result($stmt, $userID, $userName, $pwd);
+                
+                if(mysqli_stmt_fetch($stmt)){
+                    if(password_verify($password, $hashed_password)){
+                        // Password is correct, so start a new session
+                        session_start();
+
+                         // Store data in session variables
+                         $_SESSION["loggedin"] = true;
+                         $_SESSION["userID"] = $id;
+                         $_SESSION["username"] = $username;  
+                         
+                    }
+                }
+            }
+        }   
+    }
+}
+// Redirect user to welcome page
+header("location: ../homepage.php?login=success");
+
+?>
--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -4,14 +4,46 @@
     $first = mysqli_real_escape_string($conn, $_POST['first']);
     $last = mysqli_real_escape_string($conn, $_POST['last']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $uid = mysqli_real_escape_string($conn, $_POST['uid']);
+    $userName = mysqli_real_escape_string($conn, $_POST['userName']);
     $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
 
+    $hashed_password = sha1($pwd);
+
     // if(isset($_POST['submit'])) {
-    //     $dateClicked = date('Y-m-d H:i:s');
+    //     if (empty($_POST["first"])) {
+    //         $nameErr = "First name is required";
+    //       } else {
+    //         $first = mysqli_real_escape_string($conn, $_POST['first']);
+    //     }
+        
+    //     if (empty($_POST["last"])) {
+    //         $nameErr = "Last name is required";
+    //       } else {
+    //         $last = mysqli_real_escape_string($conn, $_POST['last']);;
+    //     }
+
+    //     if (empty($_POST["email"])) {
+    //         $nameErr = "Email is required";
+    //       } else {
+    //         $email = mysqli_real_escape_string($conn, $_POST['email']);
+    //     }
+
+    //     if (empty($_POST["uid"])) {
+    //         $nameErr = "Username is required";
+    //       } else {
+    //         $uid = mysqli_real_escape_string($conn, $_POST['uid']);
+    //     }
+
+    //     if (empty($_POST["pwd"])) {
+    //         $nameErr = "Password is required";
+    //       } else {
+    //         $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+    //     }
     // }
 
-    $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$first','$last', '$email', '$uid', '$pwd');";
+    
+
+    $sql = "INSERT INTO users (user_first, user_last, user_email, userName, password) VALUES ('$first','$last', '$email', '$userName', '$hashed_password');";
     mysqli_query($conn, $sql);
     // $resultCheck = mysqli_num_rows($result);
             
